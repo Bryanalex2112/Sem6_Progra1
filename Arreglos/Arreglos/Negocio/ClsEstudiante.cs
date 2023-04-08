cb3508ec99d556597b78d63199403a218f2be54c
Bryanalex2112 <bryanalvarado966@gmail.com>
2023-04-08 13:33:46 -0600

    Arreglos

---
+++ Arreglos/Arreglos/Negocio/ClsEstudiante.cs
@@ -0,0 +1,23 @@
+﻿using Arreglos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos.Negocio
+{
+    public class ClsEstudiante
+    {
+        List<Estudiante>Estudiantes = new List<Estudiante>();
+
+        public void AgregarEstudante(Estudiante est)
+        {
+            Estudiantes.Add(est);
+        }
+        public List<Estudiante> ListadoEstudiante()
+        {
+            return Estudiantes;
+        } 
+    }
+}
---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Arreglos.Entidades;
+using Arreglos.Negocio;
+
+Estudiante estudiante = new Estudiante();   
+ClsEstudiante clsEstudiante = new ClsEstudiante();
+
+estudiante.Nombre = "Brayan";
+estudiante.Apellido = "Alvarado";
+estudiante.Edad = 20;
+estudiante.Dui = "06385841-2";
+
+clsEstudiante.AgregarEstudante(estudiante);
+
+foreach (var mostrar in clsEstudiante.ListadoEstudiante())
+{
+    Console.WriteLine($"nombre->{ mostrar.Nombre}, apellido->{ mostrar.Apellido}, edad->{mostrar. Edad}, Dui->{mostrar.Dui}");
+
+    Console.WriteLine("nombre->"+mostrar.Nombre +", apellido->"+ mostrar.Apellido + ", edad->"+mostrar.Edad+", Dui->"+mostrar.Dui);
+}
